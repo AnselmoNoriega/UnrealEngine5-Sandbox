8d585dfe67fddb36d3a3413122702d97171eabbf
AnselmoNoriega <anselmonoriega3@gmail.com>
2024-02-12 15:49:16 -0800

    Half way

--- TestProject/Source/TestProject/MainController.cpp
+++ TestProject/Source/TestProject/MainController.cpp
@@ -1,5 +1,12 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "MainController.h"
+#include "MainCharacter.h"
 
+void AMainController::OnPossess(APawn* aPawn)
+{
+	Super::OnPossess(aPawn);
+}
+
+void AMainController::OnUnPossess()
+{
+	Super::OnUnPossess();
+}
--- TestProject/Source/TestProject/MainController.h
+++ TestProject/Source/TestProject/MainController.h
@@ -6,6 +6,10 @@
 #include "GameFramework/PlayerController.h"
 #include "MainController.generated.h"
 
+class UEnhancedInputComponent;
+class AMainCharacter;
+class UInputMappingContext;
+
 /**
  * 
  */
@@ -13,5 +17,25 @@ UCLASS()
 class TESTPROJECT_API AMainController : public APlayerController
 {
 	GENERATED_BODY()
-	
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player Input|Character Movement")
+	TObjectPtr<UInputAction> ActionMove = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player Input|Character Movement")
+	TObjectPtr<UInputAction> ActioLook = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player Input|Character Movement")
+	TObjectPtr<UInputAction> ActionJump = nullptr;
+
+protected:
+	virtual void OnPossess(APawn* aPawn) override;
+	virtual void OnUnPossess() override;
+
+private:
+
+	UPROPERTY()
+	TObjectPtr<UEnhancedInputComponent> mInputComponent = nullptr;
+
+	UPROPERTY()
+	TObjectPtr<AMainCharacter> player = nullptr;
 };
