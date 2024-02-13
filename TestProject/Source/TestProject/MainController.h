0839bd3727ee2aea83b766634f9024a294355533
AnselmoNoriega <anselmonoriega3@gmail.com>
2024-02-12 16:20:59 -0800

    Inputs

--- TestProject/Source/TestProject/MainController.h
+++ TestProject/Source/TestProject/MainController.h
@@ -3,15 +3,47 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputAction.h"
+#include "InputActionValue.h"
 #include "GameFramework/PlayerController.h"
 #include "MainController.generated.h"
+
+class UEnhancedInputComponent;
+class AMainCharacter;
+class UInputMappingContext;
 
 /**
  * 
  */
 UCLASS()
-class TESTPROJECT_API AMainController : public APlayerController
+class AMainController : public APlayerController
 {
 	GENERATED_BODY()
-	
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player Input|Character Movement")
+	TObjectPtr<UInputAction> ActionMove = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player Input|Character Movement")
+	TObjectPtr<UInputAction> ActionLook = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player Input|Character Movement")
+	TObjectPtr<UInputAction> ActionJump = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player Input|Character Movement")
+	TObjectPtr<UInputMappingContext> InputMappingContent = nullptr;
+
+protected:
+	void HandleLook(const FInputActionValue& InputActionValue);
+	void HandleMove(const FInputActionValue& InputActionValue);
+	void HandleJump();
+
+	virtual void OnPossess(APawn* aPawn) override;
+	virtual void OnUnPossess() override;
+
+private:
+	UPROPERTY()
+	TObjectPtr<UEnhancedInputComponent> mInputComponent = nullptr;
+
+	UPROPERTY()
+	TObjectPtr<AMainCharacter> playerCharacter = nullptr;
 };
