b427f3a2e0e1bdaadd0e603743766bf20c3d62a7
AnselmoNoriega <anselmonoriega3@gmail.com>
2025-02-07 08:43:12 -0800

    Setting new project ue5

---
+++ AI_FirstPerson/Source/AI_FirstPerson/AI_FirstPersonGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AI_FirstPersonGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAI_FirstPersonGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAI_FirstPersonGameMode();
+};
+
+
+
--- HelloUE5/Source/HelloUE5/Game/FunctionUtils.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "FunctionUtils.h"
-
-void UFunctionUtils::MovePosition(FVector startPos, AActor* actor, const FVector timePosition)
-{
-    actor->SetActorLocation(startPos + timePosition);
-}
