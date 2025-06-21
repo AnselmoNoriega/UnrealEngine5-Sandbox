67d3c4a4a9c652679eaa83931e42fa7dfac758a8
AnselmoNoriega <anselmonoriega3@gmail.com>
2025-06-20 18:39:52 -0700

    Making the UI visible in game

---
+++ UI_Test/Source/UI_Test/Helpers/TestHelperBase.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TestHelperBase.h"
+
+#include "Blueprint/UserWidget.h"
+
+// Sets default values
+ATestHelperBase::ATestHelperBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATestHelperBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	APlayerController* pc = GetWorld()->GetFirstPlayerController();
+	pc->SetInputMode(FInputModeGameAndUI());
+	pc->bShowMouseCursor = true;
+
+	mCharacterSelectWidget = CreateWidget<UUserWidget>(pc, CharacterSelectClass);
+
+	mCharacterSelectWidget->AddToViewport();
+}
+
+// Called every frame
+void ATestHelperBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
